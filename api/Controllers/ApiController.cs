3ae279ddf89bf8740a0c629c2b2bcf4f4306c71d
Bethuel Dela Cruz <jbethueldc@gmail.com>
2023-08-26 19:41:27 +0800

    feat(API): auth0 authenticated request

---
+++ api/Controllers/ApiController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api")]
+public class ApiController : Controller
+{
+    [HttpGet("private-scoped")]
+    [Authorize("read")]
+    public IActionResult Scoped()
+    {
+        return Ok(
+            new
+            {
+                Message = "Hello from a private endpoint! You need to be authenticated and have a scope of read to see this."
+            }
+        );
+    }
+}
--- api/Program.cs
+++ api/Program.cs
@@ -1,21 +1,50 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
+var domain = $"https://{builder.Configuration["Auth0:Domain"]}/";
+var audience = builder.Configuration["Auth0:Audience"];
 
 builder.Services.AddRouting(options =>
 {
     options.LowercaseUrls = true;
     options.LowercaseQueryStrings = true;
 });
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = domain;
+        options.Audience = audience;
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy(
+        "read",
+        policy => policy.Requirements.Add(new HasScopeRequirement("read", domain))
+    );
+});
+
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-app.MapGet("/greet", () => "Hello World! " + DateTime.Now);
+
+app.UseRouting();
+
+app.UseAuthentication();
 app.UseAuthorization();
-app.MapControllers();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/greet", () => "Hello World! " + DateTime.Now);
 app.Run();
