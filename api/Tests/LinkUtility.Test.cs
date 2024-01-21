0d47e2f5ac1d3af1d7cee2b21582b8f5925e4bea
Bethuel Dela Cruz <jbethueldc@gmail.com>
2024-01-21 17:05:34 +0800

    setup UT

---
+++ api/Tests/LinkUtility.Test.cs
@@ -0,0 +1,34 @@
+using Xunit;
+
+namespace api.Tests
+{
+    public class LinkServiceTest
+    {
+        private readonly LinkUtility _linkUtility;
+
+        public LinkServiceTest()
+        {
+            _linkUtility = new LinkUtility();
+        }
+
+        [Theory]
+        [InlineData("https://fb.com")]
+        [InlineData("https://reddit.com")]
+        [InlineData("https://jbethuel.com")]
+        public void IsValidLink(string link)
+        {
+            var result = _linkUtility.IsValidLink(link);
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("the quick brown fox")]
+        public void IsInvalidLink(string link)
+        {
+            var result = _linkUtility.IsValidLink(link);
+            Console.WriteLine(result);
+            Assert.False(result);
+        }
+       
+    }
+}
---
+++ api/Utils/LinksUtility.cs
@@ -0,0 +1,13 @@
+using System.Text.RegularExpressions;
+
+namespace api.Tests;
+
+public class LinkUtility
+{
+  public bool IsValidLink(string payload)
+    {
+        string Pattern = @"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$";
+        Regex Rgx = new(Pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        return Rgx.IsMatch(payload);
+    }
+}
